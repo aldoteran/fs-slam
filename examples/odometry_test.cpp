7b5b2e4be1cafea7feb655356637e9cdc2eec075
antonio teran <teran@mit.edu>
2020-04-21 14:29:20 -0400

    Add examples dir with exec.

---
+++ examples/odometry_test.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file odometry_test.cpp
+ * @brief ROS node for SLAM.
+ * @date Mar 30, 2020
+ * @author tonio terán (teran@mit.edu)
+ * @author aldo terán (aldot@kth.se)
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "Simple odometry chain tests." << std::endl;
+
+  return 0;
+}
